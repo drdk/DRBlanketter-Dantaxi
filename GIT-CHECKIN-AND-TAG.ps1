eef7ff4018d88fea91aed9f15accb2de84ce9dae
rujodr <103261346+rujodr@users.noreply.github.com>
2023-10-25 10:22:34 +0200

    Lagt i test

--- GIT-CHECKIN-AND-TAG.ps1
+++ GIT-CHECKIN-AND-TAG.ps1
@@ -1,7 +1,7 @@
 git pull
 git add --all
-git commit -m "Run build 1.0.0.12" 
+git commit -m "Lagt i test" 
 git push origin
-git tag "Lagt i test"
+git tag "1.0.0.12"
 git push origin --tags
 git log --pretty=oneline
